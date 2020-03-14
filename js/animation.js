d9ba0cc14efbff4621a6b5b3879cc0d471db411d
kcamposr021 <kcamposr021@ulacit.ed.cr>
2020-03-14 09:09:51 -0600

    agregue formulario de jostin a JoinOurTeam

--- js/animation.js
+++ js/animation.js
@@ -1,4 +1,23 @@
-const descriptionBotton = document.querySelector(".descrip-combo");
-descriptionBotton.addEventListener("mouseover", () => {
+const items = document.querySelector(".item");
+const item1 = document.querySelector(".item1");
+const item2 = document.querySelector(".item2");
+const item3 = document.querySelector(".item3");
+const img1 = document.querySelector(".img1");
 
+const descrip = document.querySelector(".descrip-combo");
+items.addEventListener("mouseover", (e) => {
+
+    if (items.classList.contains("item1")) {
+        descrip.style.display = "block"
+        descript.style.position = "absolute"
+        descript.style.right = "20px"
+        img1.style.width = "20%"
+
+    }
+    if (items.classList.contains("item2")) {
+
+    }
+    if (items.classList.contains("item3")) {
+
+    }
 });
