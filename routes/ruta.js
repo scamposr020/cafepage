edfe1d0fa48b6535095b4972c338a8bc0b8d53eb
scamposr020 <scamposr020@ulacit.ed.cr>
2020-03-23 11:04:52 -0600

    Rama Actualizada

--- routes/ruta.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-
-const todo = require('../controllers/todo');
-
-router.get('/', todo.list);
-router.post('/add', todo.save);
-
-module.exports = router;
