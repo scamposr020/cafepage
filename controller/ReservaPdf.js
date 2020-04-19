c48140f4be57b90c9b736ad7de28e8a55d9a2418
scamposr020 <scamposr020@ulacit.ed.cr>
2020-04-18 18:35:03 -0600

    prueba de pdf fallida :(

--- controller/ReservaPdf.js
+++ controller/ReservaPdf.js
@@ -11,13 +11,52 @@ controller.PDFgenerated = (req, res) => {
         });
     })
 
-    document.getElementById("imprimir").addEventListener("click", DescargaPdf);
+    var pdf = require("pdf-creator-node");
+    var fs = require('fs');
 
-    function DescargaPdf() {
-        var doc = new jsPDF();
-        doc.text("holi");
-        doc.save("output.pdf");
-    }
-    DescargaPdf();
+    // Read HTML Template
+    var html = fs.readFileSync('Menu-Admin.ejs', 'utf8');
+
+    var options = {
+        format: "A3",
+        orientation: "portrait",
+        border: "10mm",
+        header: {
+            height: "45mm",
+            contents: '<div style="text-align: center;">Author: Shyam Hajare</div>'
+        },
+        "footer": {
+            "height": "28mm",
+            "contents": {
+                first: 'Cover page',
+                2: 'Second page', // Any page number is working. 1-based index
+                default: '<span style="color: #444;">{{page}}</span>/<span>{{pages}}</span>', // fallback value
+                last: 'Last Page'
+            }
+        }
+
+    };
+    var users = [{
+            name: "Shyam",
+            age: "26"
+        },
+        {
+            name: "Navjot",
+            age: "26"
+        },
+        {
+            name: "Vitthal",
+            age: "26"
+        }
+    ]
+    var document = {
+        html: html,
+        data: {
+            users: users
+        },
+        path: "./output.pdf"
+    };
+
+
 }
 module.exports = controller;
